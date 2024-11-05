b2846657ea5ea0be75231a3eab2a4fab9d1c73f6
Hanusha <u22cs066@coed.svnit.ac.in>
2024-11-05 18:11:21 +0530

    domain

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-    domain="dev-kv1eujmgsnmfonl3.us.auth0.com"
-    clientId="Hpd7ilmirJO7TwDbqeDpCPIItwThjL34"
+    domain=""
+    clientId=""
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
